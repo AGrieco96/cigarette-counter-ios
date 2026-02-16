1789976e2503db8aedf034e51493d02c0921088a
AGrieco96 <100705990+AGrieco96@users.noreply.github.com>
2026-02-16 18:29:40 +0100

    Unregister stale service workers to avoid cached Pages routing issues

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,17 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'sonner';
-import { registerSW } from 'virtual:pwa-register';
 import App from './App';
 import './index.css';
 import { AuthProvider } from './hooks/useAuth';
 
-registerSW({ immediate: true });
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => {
+      registration.unregister();
+    });
+  });
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
       </BrowserRouter>
       <Toaster richColors position="top-center" />
